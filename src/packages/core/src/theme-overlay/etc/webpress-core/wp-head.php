ce59dd3c3d14090de386cc2d18490018015f08cb
Will Haynes <will@wjh.dev>
2022-02-02 07:39:56 -0600

    optional stencil-stat cache busting (#6)

--- src/packages/core/src/theme-overlay/etc/webpress-core/wp-head.php
+++ src/packages/core/src/theme-overlay/etc/webpress-core/wp-head.php
@@ -2,14 +2,25 @@
 
 function _webpress_print_wp_head() {
     global $WPContext;
+
+    $json = file_get_contents( "/srv/stencil-stats.json");
+    $varParam;
+    if ($json) {
+        $decoded = json_decode($json);
+        $varParam = "?v=" . substr(md5($decoded->timestamp),0,8);
+    } else {
+        $varParam = "";
+    }
     ?>
 
 <script type="text/javascript">
 var webpress = <?php echo json_encode($WPContext) ?>
 </script>
 <script type="module"
-    src="<?php echo get_template_directory_uri() . '/app/' . WEBPRESS_STENCIL_NAMESPACE . '.esm.js' ?>"></script>
-<script nomodule src="<?php echo get_template_directory_uri() . '/app/' . WEBPRESS_STENCIL_NAMESPACE . '.js' ?>">
+    src="<?php echo get_template_directory_uri() . '/app/' . WEBPRESS_STENCIL_NAMESPACE . '.esm.js' . $varParam ?>">
+</script>
+<script nomodule
+    src="<?php echo get_template_directory_uri() . '/app/' . WEBPRESS_STENCIL_NAMESPACE . '.js' . $varParam ?>">
 </script>
 
 <?php
