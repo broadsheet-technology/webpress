a0c3d260bf7dd45e7c558cb6d4fbf267ba4b2e29
Will Haynes <will@wjh.dev>
2023-04-22 17:48:09 -0500

    init → after_setup_theme

--- src/packages/core/src/theme-overlay/etc/webpress-core/init.php
+++ src/packages/core/src/theme-overlay/etc/webpress-core/init.php
@@ -4,16 +4,16 @@
  * Registers theme menus and features from
  * theme-defition.json
  */
-function _init_webpress() {
+function _init_webpress()
+{
     $theme = webpress_get_theme_definition();
 
-    foreach( $theme->menus as $menu ) {
-        register_nav_menu( $menu->location, $menu->description );
+    foreach ($theme->menus as $menu) {
+        register_nav_menu($menu->location, $menu->description);
     }
 
-    foreach( $theme->features as $feature ) {
-        add_theme_support( $feature );
+    foreach ($theme->features as $feature) {
+        add_theme_support($feature);
     }
 }
-add_action( 'init' , '_init_webpress' );
-add_action( 'admin_init' , '_init_webpress' );
+add_action('after_setup_theme', 'register_my_menu');
